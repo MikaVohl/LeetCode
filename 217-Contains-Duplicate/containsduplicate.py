7fd0d7fd961d7bc82107fa0e1a8418162ad73edf
Mika Vohl <mikavohl@gmail.com>
2024-09-18 23:15:35 -0400

    Added 217-Contains-Duplicate solution

---
+++ 217-Contains-Duplicate/containsduplicate.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        appeared = set()
+        for num in nums:
+            if num in appeared:
+                return True
+            appeared.add(num)
+        return False
