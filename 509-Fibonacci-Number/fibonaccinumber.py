a8d8c88a16eac50cb99398046a57ba7d58f60eaf
Mika Vohl <mikavohl@gmail.com>
2024-10-16 10:51:41 -0400

    Added 509-Fibonacci-Number Solution O(n)

--- 509-Fibonacci-Number/fibonaccinumber.py
+++ 509-Fibonacci-Number/fibonaccinumber.py
@@ -1,4 +1,17 @@
+# Time Complexity: O(2^n)
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n < 2: return n
+#         return self.fib(n-1) + self.fib(n-2)
+
+# Time Complexity: O(n)
+computations = {}
+
 class Solution:
-    def fib(self, n: int) -> int:
+    def fib(self, n:int) -> int:
         if n < 2: return n
-        return Solution.fib(self, n-1) + Solution.fib(self, n-2)
+        if n in computations:
+            return computations[n]
+        result = self.fib(n-1) + self.fib(n-2)
+        computations[n] = result
+        return result
