32512241fe078d9421ab5f13b63975330b5897ef
Mika Vohl <mikavohl@gmail.com>
2024-10-17 09:52:34 -0400

    Finished problem 371

--- 371-Sum-of-Two-Integers/sumoftwointegers.py
+++ 371-Sum-of-Two-Integers/sumoftwointegers.py
@@ -1,17 +1,11 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        a = bin(a)[2:]
-        b = bin(b)[2:]
-        result = ''
-        carry = False
-        for i in range(-1, -1 * max(len(a), len(b)) - 2, -1):
-            current = 0
-            if carry:
-                current += 1
-            if len(a) >= (-1 * i) and a[i] == '1':
-                current += 1
-            if len(b) >= (-1 * i) and b[i] == '1':
-                current += 1
-            result = str(current % 2) + result
-            carry = current // 2 != 0
-        return int(result, 2)
+        # 32 bit mask. Expressed in hexadecimal
+        mask = 0xffffffff
+
+        while (b & mask) > 0:
+            add = a ^ b
+            carry = (a & b) << 1
+            a = add
+            b = carry
+        return (a & mask) if b > 0 else a
